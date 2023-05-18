53708e0d3805bfa7d00fce9ea4e381c772a03ffc
yasinarafat9 <yasinarafat9612@gmail.com>
2023-05-18 17:42:33 +0600

    added register page

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,31 +1,37 @@
+import './Register.css'
 import { Link } from 'react-router-dom';
-import './Login.css'
 import { FaGoogle, FaTwitter } from "react-icons/fa";
-const Login = () => {
+const Register = () => {
     return (
         <div className="mx-auto">
             <div className="mx-auto form-box">
                 <form className="">
-                    <div className=''>
+                    <div>
+                        <h5>Your Name:</h5>
+                        <input type="text" name="name" placeholder="Enter your Name" />
+                    </div>
+                    <br />
+                    <div>
                         <h5>Email:</h5>
-                        <input type="text" name="email" placeholder="Enter your Email" />
+                        <input type="eamil" name="email" placeholder="Enter your Email" />
                     </div>
                     <br />
                     <div>
                         <h5>Password:</h5>
                         <input type="password" name="password" placeholder="Enter Password" />
                         <br />
-                        <button className='btn-web my-4 mx-auto'>Login</button>
+                        <button className='btn-web my-4 mx-auto'>Register</button>
                     </div>
                     <div>
                         <div className='text-center my-3'>
-                            <p className='my-2'>Or login Using..</p>
+                            <p className='my-2'>Or login Using..
                             <FaGoogle className='text-danger fs-2 m-2' />
                             <FaTwitter className='text-primary fs-2 m-2' />
-                            <p>New to this website? <Link to='/register'>Please Register</Link></p>
+                            </p>
                             
+                            <p>Already have an Account?<Link to='/login'>Please Login</Link></p>
                         </div>
-                        
+
                     </div>
                 </form>
             </div>
@@ -33,4 +39,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default Register;
